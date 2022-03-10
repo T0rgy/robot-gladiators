0b84b68474104fe8ed521ebf1d2538d921cd64bb
Torgy <jctorge@gmail.com>
2022-03-09 21:54:53 -0600

    random enemy order

--- assets/js/game.js
+++ assets/js/game.js
@@ -38,19 +38,35 @@ var fightOrSkip = function() {
 }
 
 var fight = function(enemy) {
+  //keep track of who goes first
+  var isPlayerTurn = true;
+
+    // randomly change turn order
+    if (Math.random() > 0.5) {
+      isPlayerTurn = false;
+    }
+
     while (playerInfo.health > 0 && enemy.health > 0) {
+      if (isPlayerTurn) {
       // ask player if they'd like to fight or skip using fightOrSkip function
       if (fightOrSkip()) {
         // if true, leave fight by breaking loop
         break;
       } 
 
-      //generate random damage value based on player's attack power
       var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
+      // remove enemy's health by subtracting the amount we set in the damage variable
       enemy.health = Math.max(0, enemy.health - damage);
       console.log(
-        playerInfo.name + ' attacked ' + enemy.name + '. ' + enemy.name + ' now has ' + enemy.health + ' health remaining.'
+        playerInfo.name +
+          " attacked " +
+          enemy.name +
+          ". " +
+          enemy.name +
+          " now has " +
+          enemy.health +
+          " health remaining."
       );
   
       // check enemy's health
@@ -59,12 +75,16 @@ var fight = function(enemy) {
   
         // award player money for winning
         playerInfo.money = playerInfo.money + 20;
+
         // leave while() loop since enemy is dead
         break;
       } else {
         window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
       }
+      //okater gets attacked first
+    } else {
       var damage = randomNumber(enemy.attack -3, enemy.attack);
+
       // remove players's health by subtracting the amount set in the damage variable
       playerInfo.health = Math.max(0, playerInfo.health - damage);
       console.log(
@@ -79,8 +99,12 @@ var fight = function(enemy) {
       } else {
         window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
       }
-    } // end of while loop
-  }; // end of fight function
+    } 
+    // switch turn order for next round
+    isPlayerTurn =!isPlayerTurn;
+  }
+    // end of while loop
+}; // end of fight function
 
 
 // function to start a new game
